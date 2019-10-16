076bc5fb5244b01138cc554844493a6a705dd30c
SimonCheng <simon_ccheng@vipabc.com>
2019-10-16 14:37:10 +0800

    tableview demo

--- Podfile
+++ Podfile
@@ -2,7 +2,8 @@ platform :ios, '9.0'
 
 target 'RxSwiftDemo' do
   use_frameworks!
-	pod 'RxSwift', '~> 5.0'
-  pod 'RxCocoa', '~> 5.0'
+	pod 'RxSwift'
+  pod 'RxCocoa'
+  pod 'SnapKit'
 
 end
--- RxSwiftDemo/ViewController.swift
+++ RxSwiftDemo/ViewController.swift
@@ -7,14 +7,40 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
+    
+    lazy var tableView: UITableView = {
+        let temp = UITableView(frame: CGRect.zero, style: .plain)
+        temp.register(UITableViewCell.self, forCellReuseIdentifier: "UITableViewCell")
+        temp.dataSource = self
+        return temp
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(tableView)
+        tableView.snp_makeConstraints { (make) in
+            make.edges.equalTo(view)
+        }
     }
-
-
 }
 
+
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell")
+        cell?.textLabel?.text = "cxc"
+        return cell ?? UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+}
